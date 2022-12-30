3d034b36e074941454e90a0ff3d70b1f77346c19
YuvalDP3129 <yuval@nclouds.com>
2022-12-30 20:25:49 +0530

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders Modules header', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Modules/i);
-  expect(linkElement).toBeInTheDocument();
-});
